526ef7410ef909e8323f32a4b4bce5e2dc83e569
mormolis <george.lamprakis@gmail.com>
2017-10-25 17:25:29 +0100

    beautifies code

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -1,91 +1,95 @@
-describe("Thermostat", function(){
+describe("Thermostat", function () {
   var thermostat;
 
-  beforeEach(function(){
+  beforeEach(function () {
     thermostat = new Thermostat();
   });
 
-  it("should have a default temperature value of 20 degrees", function(){
+  it("should have a default temperature value of 20 degrees", function () {
     expect(thermostat.temperature).toEqual(20);
   });
 
-  it("#upTemperature should raise the temperature by 1 degree", function(){
+  it("#upTemperature should raise the temperature by 1 degree", function () {
     thermostat.upTemperature();
     expect(thermostat.temperature).toEqual(21);
   });
 
-  it("#downTemperature should drop the temperature by 1 degree", function(){
+  it("#downTemperature should drop the temperature by 1 degree", function () {
     thermostat.downTemperature();
     expect(thermostat.temperature).toEqual(19);
   });
 
-  it("should not allow the temperature fall bellow 10 degrees", function(){
-    for(var i = 0; i <10; i++){
+  it("should not allow the temperature fall bellow 10 degrees", function () {
+    for (var i = 0; i < 10; i++) {
       thermostat.downTemperature();
     }
-    expect(function(){thermostat.downTemperature()}).toThrowError("Temperature cannot fall bellow 10");
+    expect(function () {
+      thermostat.downTemperature()
+    }).toThrowError("Temperature cannot fall bellow 10");
   });
 
-  it("#setPowerSavingOn should turn on power saving mode", function(){
+  it("#setPowerSavingOn should turn on power saving mode", function () {
     thermostat.setPowerSavingOn();
     expect(thermostat.isPowerSavingOn()).toEqual(true);
   });
 
-  it("does not allow temperature to be more than 25 degrees when power saving mode is on", function(){
+  it("does not allow temperature to be more than 25 degrees when power saving mode is on", function () {
     thermostat.setPowerSavingOn();
-    for(var i = 0; i < 5; i++){
+    for (var i = 0; i < 5; i++) {
       thermostat.upTemperature();
     }
-    expect(function(){ thermostat.upTemperature() }).toThrowError("Power Saving Mode ON, temperature cannot be greater than 25");
+    expect(function () {
+      thermostat.upTemperature()
+    }).toThrowError("Power Saving Mode ON, temperature cannot be greater than 25");
   });
 
-  it("allows temperature to be more than 25 degrees when power saving mode is off", function(){
+  it("allows temperature to be more than 25 degrees when power saving mode is off", function () {
     thermostat.setPowerSavingOff();
-    for(var i = 0; i <= 5; i++){
+    for (var i = 0; i <= 5; i++) {
       thermostat.upTemperature();
     }
     expect(thermostat.temperature).toEqual(26);
   });
 
-  it("does not allow temperature to be more than 32 degrees when power saving mode is off", function(){
+  it("does not allow temperature to be more than 32 degrees when power saving mode is off", function () {
     thermostat.setPowerSavingOff();
-    for(var i=0; i<12; i++){
+    for (var i = 0; i < 12; i++) {
       thermostat.upTemperature();
     }
-    expect(function(){thermostat.upTemperature()}).toThrowError("Cannot increase the temperature above 32")
+    expect(function () {
+      thermostat.upTemperature()
+    }).toThrowError("Cannot increase the temperature above 32")
   });
 
-  it("#reset resets the temperature to 20 degrees", function(){
-    for(var i=0; i<4; i++){
+  it("#reset resets the temperature to 20 degrees", function () {
+    for (var i = 0; i < 4; i++) {
       thermostat.upTemperature();
     }
     thermostat.reset();
     expect(thermostat.temperature).toEqual(20);
   });
 
-  it("#energyUsage should return 'low-usage' if temperature is bellow 18", function(){
-    for(var i=0; i<4; i++){
+  it("#energyUsage should return 'low-usage' if temperature is bellow 18", function () {
+    for (var i = 0; i < 4; i++) {
       thermostat.downTemperature();
     }
     console.log("low", thermostat.temperature);
     expect(thermostat.energyUsage()).toEqual("low-usage");
   });
 
-  it("#energyUsage should return 'medium-usage' if temperature is bellow 25 and above 18", function(){
-    for(var i=0; i<4; i++){
+  it("#energyUsage should return 'medium-usage' if temperature is bellow 25 and above 18", function () {
+    for (var i = 0; i < 4; i++) {
       thermostat.upTemperature();
     }
     console.log("medium", thermostat.temperature);
     expect(thermostat.energyUsage()).toEqual("medium-usage");
   });
 
-  it("#energyUsage should return 'high-usage' if temperature is 25 and above", function(){
-    for(var i=0; i<=4; i++){
+  it("#energyUsage should return 'high-usage' if temperature is 25 and above", function () {
+    for (var i = 0; i <= 4; i++) {
       thermostat.upTemperature();
     }
     console.log("high", thermostat.temperature);
     expect(thermostat.energyUsage()).toEqual("high-usage");
   });
-
-
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,42 +1,42 @@
-function Thermostat(){
+function Thermostat() {
   this.MINIMUM_TEMPERATURE = 10;
   this.MAXIMUM_TEMPERATURE = 32;
   this.POWER_SAVING_MAX_TEMPERATURE = 25;
   this.DEFAULT_TEMPERATURE = 20;
-  
+
   this.temperature = 20;
   this.setPowerSavingOn();
-} 
+}
 
-Thermostat.prototype.upTemperature = function(){
-  if(this.isPowerSavingOn() && this.temperature === this.POWER_SAVING_MAX_TEMPERATURE) throw Error("Power Saving Mode ON, temperature cannot be greater than 25") ;
-  if(this.temperature === this.MAXIMUM_TEMPERATURE) throw Error("Cannot increase the temperature above 32");
+Thermostat.prototype.upTemperature = function () {
+  if (this.isPowerSavingOn() && this.temperature === this.POWER_SAVING_MAX_TEMPERATURE) throw Error("Power Saving Mode ON, temperature cannot be greater than 25");
+  if (this.temperature === this.MAXIMUM_TEMPERATURE) throw Error("Cannot increase the temperature above 32");
   this.temperature += 1;
 }
 
-Thermostat.prototype.downTemperature = function(){
+Thermostat.prototype.downTemperature = function () {
   if (this.temperature == this.MINIMUM_TEMPERATURE) throw Error("Temperature cannot fall bellow 10");
   this.temperature -= 1;
 }
 
-Thermostat.prototype.setPowerSavingOn = function(){
+Thermostat.prototype.setPowerSavingOn = function () {
   this._powerSaving = true;
 }
 
-Thermostat.prototype.isPowerSavingOn = function() {
+Thermostat.prototype.isPowerSavingOn = function () {
   return this._powerSaving;
 }
 
-Thermostat.prototype.setPowerSavingOff = function() {
+Thermostat.prototype.setPowerSavingOff = function () {
   this._powerSaving = false;
 }
 
-Thermostat.prototype.reset = function() {
+Thermostat.prototype.reset = function () {
   this.temperature = this.DEFAULT_TEMPERATURE;
 }
 
-Thermostat.prototype.energyUsage = function(){
-  if(this.temperature < 18) return "low-usage";
-  if(this.temperature < 25) return "medium-usage";
+Thermostat.prototype.energyUsage = function () {
+  if (this.temperature < 18) return "low-usage";
+  if (this.temperature < 25) return "medium-usage";
   return "high-usage";
 }
